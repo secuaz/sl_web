82b46f354c51ab42cbf7bf743b895c9341561207
rodrigo-vallejos <rodrigo-vallejos@outlook.com>
2024-11-24 18:43:53 -0300

    - Procesado de imagenes - Creado listado de productos con archivo JSON y procesados con JS para insertarlo en HTML dinámicamente. - Creado el modal para los productos.

---
+++ assets/js/products.js
@@ -0,0 +1,68 @@
+fetch('../../products.json')
+    .then(response => response.json())
+    .then(products => {
+        console.log(products)
+        products.forEach(product => {
+            const productHTML = `
+        <!-- TARJETA -->
+            <div class="card">
+                <a id="modalBtn" class="btn" href="#open-modal">
+                    <img src="${product.imgFile}" alt="${product.altDescription}">
+                </a>
+                <div class="card-container">
+                    <h4><b>${product.productName}</b></h4>
+                    <p>${product.productCode}</p>
+                </div>
+            </div>
+
+        <!-- MODAL -->
+        <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Cerrar" class="modal-close">Close</a>
+
+                        <div class="modal-content">
+                            <div class="content-img">
+                                <h1>${product.productName}</h1>
+                                <img src="${product.imgFile}">
+                            </div>
+                            <div class="">
+                                <div class="tec-content">
+                                    <div class="post-content">
+                                        <p><strong>Información Técnica</strong></p>
+
+                                    </div>
+                                    <div class="tec-detail">
+
+                                        <table>
+                                            <tr>
+                                                <th>Código:</th>
+                                                <td>${product.productCode}</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Medida:</th>
+                                                <td>${product.productName}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Ancho:</th>
+                                                <td>${product.width}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Diámetro</th>
+                                                <td>${product.diameter}</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div>
+                                        <a rel="lightbox" href="#"> 
+                                            <img src="${product.imgTec}">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+            </div>
+        </div>
+        `;
+            document.getElementById('card-list').innerHTML += productHTML;
+        });
+    });
